0b3976d5eb331540663be22f9a159a592ad2698d
roesleinn <shuangweishu@outlook.com>
2020-07-02 00:07:41 +0200

    1

---
+++ app/src/main/java/com/g15/demo/ui/main/Fragment2.java
@@ -0,0 +1,25 @@
+package com.g15.demo.ui.main;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.g15.demo.R;
+
+public class Fragment2 extends Fragment {
+
+    @Override
+    public View onCreateView(
+            @NonNull LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment2, container, false);
+    }
+}
---
+++ app/src/main/java/com/g15/demo/ui/main/SectionsPagerAdapter.java
@@ -0,0 +1,54 @@
+package com.g15.demo.ui.main;
+
+import android.content.Context;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.StringRes;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import com.g15.demo.R;
+
+/**
+ * A [FragmentPagerAdapter] that returns a fragment corresponding to
+ * one of the sections/tabs/pages.
+ */
+public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+    //private String[] mTitles = new String[]{"Settings", "Context"};
+    @StringRes
+    private static final int[] TAB_TITLES = new int[]{R.string.tab_text_1, R.string.tab_text_2};
+    private final Context mContext;
+
+    public SectionsPagerAdapter(Context context, FragmentManager fm) {
+        super(fm);
+        mContext = context;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        // getItem is called to instantiate the fragment for the given page.
+        // Return a PlaceholderFragment (defined as a static inner class below).
+        if (position == 1) {
+            return new Fragment2();
+        } else {
+            return new Fragment1();
+        }
+        // return Fragment1.newInstance(position + 1);
+    }
+
+    @Override
+    public int getCount() {
+        //return 2;
+        //return mTitles.length;
+        return TAB_TITLES.length;
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return mContext.getResources().getString(TAB_TITLES[position]);
+        //return mTitles[position];
+    }
+}
